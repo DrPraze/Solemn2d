e70527ee80d7fe940d51a55b959ae0b31eed5c30
Praise James <66879837+Trojan-Cipher@users.noreply.github.com>
2021-01-18 17:09:39 +0100

    updated

--- skeleton_demo.py
+++ skeleton_demo.py
@@ -1,17 +1,28 @@
 from tkinter import *
 from tkinter.filedialog import *
 from tkinter.messagebox import *
-from TkinterDnD2 import *
-#from glob import glob #for testing purposes
+#from TkinterDnD2 import *
+from glob import glob #for testing purposes
 import pygame
 from time import sleep
 import os
-import smtplib, ssl
 from tkinter import ttk
+from PIL import ImageTk
+from tooltip import *
+from scrollimage import ScrollableImage
+from animator import anime
 
-root = TkinterDnD.Tk()
+#root = TkinterDnD.Tk()
+root = Tk()
 pygame.init()
-images = []
+"""
+_next_ = ImageTk.PhotoImage(file = 'imgs/next.png')
+images = [_next_]
+with open('filename.spf', 'w+') as f:
+    text = str(images)
+    f.write(text)
+"""
+
 class Skeleton:
     global pic
     menuBar = Menu(root)
@@ -24,299 +35,168 @@ class Skeleton:
         pic = Label(root, image = Pic)
         pic.place(x = 230, y = 100)
     except:pass
-    sound_track = ''
+    _next_ = ImageTk.PhotoImage(file = 'imgs/super.png')
+    images = [_next_]
+    images = ['imgs/super.png']
+    sound_track = None
     def __init__(self):
         root.geometry('850x600')
-        root.title("Untitled - Skeleton 2D 1.0")
-        root.resizable(False, False)
+        root.title("Untitled - Skeleton 2D Beta")
         root.config(bg = 'white')
-        root.wm_iconbitmap("skeleton.png")
-        
+        try:root.wm_iconbitmap("skeleton.png")
+        except:pass
         self.FileMenu.add_command(label = "Open Image", command = self.Open_image)
-        self.FileMenu.add_command(label = "Import Sound", command = self.import_sound)
+        self.FileMenu.add_command(label = "Import Sound", command = self.comingsoon)
         self.FileMenu.add_command(label = "New project", command = self.New)
         self.FileMenu.add_command(label = "Open project", command = self.Open)
         self.FileMenu.add_command(label = "Save Project", command = self.save)
-        self.FileMenu.add_command(label = "Export", command = None)
+        self.FileMenu.add_command(label = "Export", command = self.comingsoon)
         self.FileMenu.add_command(label = "Exit", command = root.destroy)
         
         self.HelpMenu.add_command(label = "About ", command = self.About)
         self.HelpMenu.add_command(label = "How to use", command = self.Use)
-        self.HelpMenu.add_command(label = "Rate Us", command =  self.Rate)
-        self.HelpMenu.add_command(label = "Send Feedback", command = self.SendFeedback)
-        self.HelpMenu.add_command(label = "Sponsor Us", command = None)
+        self.HelpMenu.add_command(label = "Rate Us", command =  self.comingsoon)
+        self.HelpMenu.add_command(label = "Send Feedback", command = self.comingsoon)
+        self.HelpMenu.add_command(label = "Sponsor Us", command = self.comingsoon)
         
-        self.ToolMenu.add_command(label = "Play", command = None)
-        self.ToolMenu.add_command(label = "Pause", command = None)
-        self.ToolMenu.add_command(label = "Animate", command = Animate)
-        self.ToolMenu.add_command(label = "Stop", command = None)
+        self.ToolMenu.add_command(label = "Play", command = self.comingsoon)
+        self.ToolMenu.add_command(label = "Pause", command = self.comingsoon)
+        self.ToolMenu.add_command(label = "Animate", command = self.Animate)
+        self.ToolMenu.add_command(label = "Stop", command = self.comingsoon)
 
         self.menuBar.add_cascade(label = "File", menu = self.FileMenu)
         self.menuBar.add_cascade(label = "Tools", menu = self.ToolMenu)
         self.menuBar.add_cascade(label = "Help", menu = self.HelpMenu)
-        root.config ( menu = self.menuBar)
 
         self.widgets()
-        file = None
+        root.config (menu = self.menuBar)
+        root.bind('<F1>', lambda x:[self.Use])
+        self.file = None
+ 
+        self.FPS = IntVar()
+        fps_label = ttk.LabelFrame(root, text = "fps", width = 100, height = 53)
+        fps_label.place(x = 340, y = 7)
+        self.fps = Spinbox(fps_label, from_ = 1, to = 150, width  = 10, bd= 2, textvariable = self.FPS)
+        self.fps.place(x = 10, y = 2)
+        create_Tip(self.fps, "Set the frames per second")
+    
+    def comingsoon(self):#for coming soon features that are not yet included
+        showinfo("HAHA, see you", "i'm still working on it, coming soon")
 
     def Valid_pic(self, filename):
         with open(filename, 'rb') as img:
             img.seek(163)
-            v = img.read(2)
-            height = (v[0] <<8) + a[1]
+            a = img.read(2)
+            height = (a[0] <<8) + a[1]
             v = img.read(2)
             width = (v[0] <<8) + v[1]
-            return list(width, height)
+            print(width)
+            print(height)
+            return [width, height]
 
     def Open_image(self):
         try:
-            self.file = askopenfilename()
+            self.file = askopenfilename(title = "Open image - Skeleton2D Beta")
         except FileNotFoundError:pass
-        if self.file == " ":
+        if self.file == '':
             self.file = None
         else:
             try:
-                f = open(self.file)
-                lb.insert(END, f)
-                images.append(f)
-            except FileNotFoundError: showinfo("Error", "hey you didn't open nothing!")
+                f = open(self.file, 'w+')
+                self.images = data
+            except Exception as e: showinfo("An Error Occured", e)
 
     def New(self):
-        root.title("Untitled - Skeleton 2D 1.0")
+        root.title("Untitled - Skeleton2D Beta")
         self.file = None
-        lb.delete(0, END)
+        self.images = []
 
     def Open(self):
-        self.file = askopenfilename(defaultextension = " .txt", filetypes = [("Text Documents" , "*.txt") ])
-        if self.file == " ":
+        self.file = askopenfilename(title = "Open recent project - Skeleton2D Beta", defaultextension = " .spf", filetypes = [("Skeleton2D Primary Format" , "*.spf") ])
+        if self.file == '':
             self.file = None
         else:
-            root.title (os.path.basename(self.file).replace('.txt', '  ') + " - Skeleton 2D 1.0")
+            root.title (os.path.basename(self.file).replace('.spf', '') + " - Skeleton2D Beta")
             try:
-                file = open(self.file, "r")                   
-                lb.insert(END, file)
+                file = open(self.file, "r")
+                data  = eval(file)
+                self.images = data
                 #file.close()
             except FileNotFoundError:
                 showinfo("ALERT", "Hey, you didn't open a file")
 
     def save(self):        
-        self.file = asksaveasfilename(initialfile = 'Untitled', defaultextension = " .txt", filetypes = [("All Files", "*.*"), ("Text Documents", "* .txt")])
+        self.file = asksaveasfilename(title = "Save Project - Skeleton2D Beta", initialfile = 'Untitled', defaultextension = " .spf", filetypes = [("Skeleton2D Primary Format", "* .spf")])
         if self.file == '':
             self.file == None
         else:
-           root.title(os.path.basename(self.file).replace('.txt', '  ') + "- Skeleton 2D 1.0")
-           with open(self.file, 'w+', encoding = 'utf-8') as f:
-               progress_ = lb.get(0, END)
+           root.title(os.path.basename(self.file).replace('.spf', '') + "- Skeleton2D Beta")
+           with open(self.file, 'w+') as f:
+               data = str(images)
                for i in progress_:
-                   f.write(i + "\n")
+                   f.write(data)
                f.close()
-               #music =
 
-    def SendFeedback(self):
-        pop = Tk()
-        pop.geometry("280x250")
-        pop.resizable(False, False)
-        pop.title("Send Feedback")
-        label = Label(pop, text = "Enter your email", fg = "black")
-        label.place(x = 10, y = 0)
-        entry = Text(pop, width = 25, height = 0)
-        entry.place(x = 10, y = 40)
-        label1 = Label(pop, text = "Enter your email password", fg = "black")
-        label.place(x = 10, y = 50)
-        entry1 = Text(pop, width = 25, height = 0)
-        entry1.place(x = 10, y = 80)
-        label2 = Label(pop, text = "Enter your feedback", fg = "black")
-        label2.place(x =10, y = 100)
-        entry2 = Text(pop, width = 25, height = 5)
-        entry2.place(x = 10, y = 120)
-        #this is just testing model, would be upgraded soon
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = entry.get(0.0, END)  # user address
-        receiver_email = "praisejames011@gmail.com" 
-        password = entry1.get(0.0, END)
-        message = entry2.get(0.0, END)
-        context = ssl.create_default_context()
-        def Send():
-            pop.destroy
-            try:
-                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-                    server.login(sender_email, password)
-                    server.sendmail(sender_email, receiver_email, message)
-            except Exception as e:
-                showerror("Error", e)
-        button = Button(pop, text = "Send", command = Send)
-        button.pack()
+    def About(self):showinfo('About', " C. Copyleft: Praise' desktop")
 
-    def About(sefl):showinfo('About', ' C. Copyleft: Novasofts Incorporations')
-
-    def Use(self):
-        showinfo("How to use", "")
-
-    def Rate(self):
-        pop = Tk()
-        pop.geometry('300x200')
-        pop.resizable(False, False)
-        pop.title("Rate Us")
-        star = PhotoImage(file = "star1.png")
-        star_ = PhotoImage(file = "star2.png")
-        def rate1():btn1.config(image =star_)
-        def rate2():
-            btn1.config(image = star_)
-            btn2.config(image = star_)
-        def rate3():
-            btn1.config(image = star_)
-            btn2.config(image =star_)
-            btn3.config(image = star_)
-        def rate4():
-            btn1.config(image = star_)
-            btn2.config(image =star_)
-            btn3.config(image = star_)
-            btn4.config(image = star_)
-        def rate5():
-            btn1.config(image =star_)
-            btn2.config(image = star_)
-            btn3.config(image = star_)
-            btn4.config(image = star_)
-            btn5.config(image = star_)
-
-        btn1 = Button(pop, image = star, command = rate1)
-        btn2 = Button(pop, image = star, command = rate2)
-        btn3 = Button(pop, image = star, command = rate3)
-        btn4 = Button(pop, image = star, command = rate4)
-        btn5 = Button(pop, image = star, command = rate5)
-        btn1.place(x = 8, y = 10)
-        btn2.place(x = 20, y = 10)
-        btn3.place(x = 40, y = 10)
-        btn4.place(x = 60, y = 10)
-        btn5.place(x = 80, y = 10)
-        
-        
-    def import_sound(self):
+    def Use(self):showinfo("How to use...", "I'll explain soon... just remind me")
+       
+    def Animate(self):
+        #progressing()
+        fps_ = self.FPS.get()
+        #work = anime()
+        images = self.images
+        anime.animate(images, float(1/fps_))
         try:
-            self.sound_track == pygame.mixer.music.load(askopenfilename())
-            self.sound.config(text = self.sound_track)
+            pygame.mixer.music.play(-1)
         except Exception as e:
-            showerror("An Error Occured", f"{e}")
+            showerror("Error", e)
+        #====================TEST===========================
+        #images = []
+        #for i  in glob("*.jpg"):
+            #images.append(i)
+        #for i in images:
+            #anime.animate(images, 1)
+        #==================END OF REGION===============
 
     def widgets(self):
-        fps_label = Label(root, text = "fps", bg = '#000', font = ("Helvetica", 15), fg = "white")
-        fps_label.place(x = 380, y = 20)
-        self.sound = Label(root, text = self.sound_track, font= ("Calibri", 20), bg = "black", fg = "white")
-        self.sound.place(x = 100, y = 480)
+        class Btn(Button):
+            def __init__(self, master, **kw):
+                Button.__init__(self, master = master, **kw)
+                self.defaultBackground = self["background"]
+                self.config(relief = GROOVE)
+                self.bind("<Enter>", self.on_enter)
+                self.bind("<Leave>", self.on_leave)
 
-class Btn(Button):
-    def __init__(self, master, **kw):
-        Button.__init__(self, master = master, **kw)
-        self.defaultBackground = self["background"]
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
+            def on_enter(self, e): self['background'] = self['activebackground']
+            def on_leave(self, e): self['background'] = self.defaultBackground
 
-    def on_enter(self, e): self['background'] = self['activebackground']
-    def on_leave(self, e): self['background'] = self.defaultBackground
+        progress_bar = ttk.Progressbar(root, orient = 'horizontal', length = 486, mode = 'determinate')
+        progress_bar.place(x = 270, y = 620)
+        #lb.drop_target_register(DND_FILES)
+        #lb.dnd_bind('<<Drop>>', file_in)
+        #lb.bind("<Button-3>", lambda x:remove(lb))
 
-class anime:
-    pygame.init()
-    def __init__(self):
-        self.screen= pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Project Preview - Skeleton 2D 1.0")
-        self.run()
+        toolbar = Canvas(root, width = 400000, height = 70).pack()
 
-    def animate(images, fps):
-        screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Project Preview - Skeleton 2D")
-        imagelist = []
-        for i in images:
-            imagelist.append(i)
-        for i in imagelist:
-            img = pygame.image.load(i)
-            screen.blit(img, [0, 0])
-            sleep(fps)
-            pygame.display.update()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                    Skeleton()
-            pygame.display.update()
-    """
-    def Test(self):
-        #====================TESTS===========================
-        images = []
-        for i  in glob("*.jpg"):
-            images.append(i)
-        for i in images:
-            self.animate(images, fps =1)
-        #==================END OF REGION===============
-    """
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                pos = pygame.mouse.get_pos()
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                    Skeleton()
-            pygame.display.update()
-            pygame.display.flip()
+        frame = ttk.LabelFrame(root, text = "Tools", width = 300, height = 53)
+        frame.place(x = 20, y = 7)
 
-def Animate():
-    progressing()
-    root.destroy()
-    fps_ = FPS.get()
-    #work = anime()
-    anime.animate(images, float(1/fps_))
-    try:
-        pygame.mixer.music.play(-1)
-    except Exception as e:
-        showerror("Error", e)
-    #====================TEST===========================
-    #images = []
-    #for i  in glob("*.jpg"):
-        #images.append(i)
-    #for i in images:
-        #anime.animate(images, 1)
-    #==================END OF REGION===============
+        btn0 = Btn(frame, text = "Prev", width = 5, height = 0, font = ('Calibri', 12), bg = '#000', foreground = '#FFF', activebackground = 'grey', highlightbackground = "#bce8f1", highlightthickness = 0.5, borderwidth = "2", command = None)
+        btn0.place(x=30, y=1, anchor = 'nw')
+        create_Tip(btn0, "Go to previous frame")
+        btn1 = Btn(frame, text = "Next", width = 5, height = 0, font = ('Calibri', 12), bg = '#000', foreground = '#FFF',  activebackground = 'grey', highlightbackground = "#bce8f1", highlightthickness = 0.5,borderwidth = "2", command = None)
+        btn1.place(x=100, y=1, anchor = 'nw')
+        create_Tip(btn1, "Go to the next frame")
+        btn2 = Btn(frame, text = "Animate", width = 7, height = 0, font = ("Calibri", 12), bg = "#000", foreground = '#fff', activebackground = 'grey', highlightbackground = "#bce8f1", highlightthickness = 0.5,borderwidth = "2", command = self.Animate)
+        btn2.place(x = 200, y = 1, anchor = 'nw')
+        create_Tip(btn2, "View animation preview")
 
-def file_in(e):
-    file = lb.tk.splitlist(e.data)[0]
-    lb.insert(END, file)
-    images.append(file)
+        img = self._next_
+        img_win = ScrollableImage(root, image = img, scrollbarwidth = 16, width = 700, height = 450)
+        img_win.place(x = 100, y = 100)
 
-def remove(lb):
-    item = lb.get(ACTIVE)
-    lb.delete(ACTIVE, item)
-
-def progressing():
-    progress_bar['maximum'] = 100
-    for i in range(101):
-        sleep(0.05)
-        progress_bar["value"] = i
-        progress_bar.update()
-    progress_bar["value"] = 0
-    showinfo("Success", "Animation compiled successfully!")
-
-progress_bar = ttk.Progressbar(root, orient = 'horizontal', length = 286, mode = 'determinate')
-progress_bar.place(x = 270, y = 420)
-frame = Frame(root)
-frame.pack(side = LEFT)
-FPS = IntVar()
-fps = Spinbox(root, from_ = 1, to = 150, width  = 10, bd= 8,textvariable = FPS)
-fps.place(x = 410, y = 20)
-
-lb = Listbox(frame, bg = "black", fg = "white", width = 35, height = 20)
-lb.pack(fill = Y, expand = 1)
-lb.drop_target_register(DND_FILES)
-lb.dnd_bind('<<Drop>>', file_in)
-lb.bind("<Button-3>", lambda x:remove(lb))
-
-btn0 = Btn(root, text = "Prev",width = 5, height = 0, font = ('Calibri', 15, 'bold', 'italic'), bg = '#000', foreground = '#FFF', activebackground = 'gold', highlightbackground = "#bce8f1", highlightthickness = 0.5, relief = RAISED, borderwidth = "2", command = None)
-btn0.place(x=690, y=20, anchor = 'nw')
-btn1 = Btn(root, text = "Next", width = 5, height = 0, font = ('Calibri', 15, 'bold', 'italic'), bg = '#000', foreground = '#FFF',  activebackground = 'gold', highlightbackground = "#bce8f1", highlightthickness = 0.5, relief = RAISED, borderwidth = "2", command = None)
-btn1.place(x=780, y=20, anchor = 'nw')
-btn2 = Btn(root, text = "Animate", width = 15, height = 0, font = ("Calibri", 15, 'bold', 'italic'), bg = "#000", foreground = '#fff', activebackground = 'gold', highlightbackground = "#bce8f1", highlightthickness = 0.5, relief = RAISED, borderwidth = "2", command = Animate)
-btn2.place(x = 500, y = 20, anchor = 'nw')
-
-#if __name__=='__main__':
-Skeleton()
+if __name__=='__main__':
+    Skeleton()
+    #Skeleton().Valid_pic('star1.png')
+    root.mainloop()
